9b136c6af1a1a2d6a4572ec49fc927ea10db50a7
Kh-Anter <105430867+Kh-Anter@users.noreply.github.com>
2022-10-16 22:27:43 +0200

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+var txt=document.getElementById("divText")
+ export  function addText(){
+
+    var h=document.createElement("h2")
+    h.innerText="Script 1"
+    txt.appendChild(h)
+}
---
+++ src/js/script2.js
@@ -0,0 +1,7 @@
+var txt=document.getElementById("divImg")
+export function addImg(){
+
+    var h=document.createElement("h2");
+     h.innerText="Script2";
+    txt.appendChild(h);
+}
